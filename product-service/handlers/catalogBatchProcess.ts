93cfdc03b6c792273bfb184338cfe016ffd4f9d9
Viktar Yarmalovich <Viktar_Yarmalovich@epam.com>
2020-11-23 02:14:00 +0300

    task-6: refactor:

--- product-service/handlers/catalogBatchProcess.ts
+++ product-service/handlers/catalogBatchProcess.ts
@@ -3,72 +3,31 @@ import 'source-map-support/register';
 import * as AWS from 'aws-sdk';
 
 export const catalogBatchProcess: SQSHandler = async (event: SQSEvent, _context, _callback) => {
+    const { IMPORT_CATALOG_SNS_ARN } = process.env;
 
     try {
         const products = event.Records.map(({body}) => body);
 
         console.log(products);
 
-        const sns = new AWS.SNS({ region: REGION });
+        const sns = new AWS.SNS();
 
         if (products && products.length) {
-        await sns
-            .publish({
-                Subject: "Imported Products",
-                Message: JSON.stringify(products),
-                TopicArn: SNS_ARN,
-                MessageAttributes: {
-                    dataValidity: {
-                    DataType: "String",
-                    StringValue: "valid",
-                    },
-                },
+            await sns
+                .publish({
+                    Subject: "Imported Products",
+                    Message: JSON.stringify(products),
+                    TopicArn: IMPORT_CATALOG_SNS_ARN 
                 })
-            .promise()
-            .then((data) => {
-                console.log(
-                    `Message ${JSON.stringify(books)} sent to the topic ${SNS_ARN}`
-                );
-                console.log("MessageID is " + data.MessageId);
+                .promise()
+                .then((data) => {
+                    console.log(`Notification ${data.MessageId} sent to the topic ${IMPORT_CATALOG_SNS_ARN}`);
                 })
-            .catch((err) => {
-                console.error(err, err.stack);
-            });
+                .catch((err) => {
+                    console.error(err, err.stack);
+                });
         }
     } catch (error) {
         return console.log(error);
     }
-
-
-//   const productsService: ProductsService = new ProductsService();
-//   const productModel: ProductModel  = JSON.parse(event.body);
-
-//   return productsService.createProduct(productModel)
-//     .then((product) => {
-//         let resultStatusCode = 400;
-//         let resultBody = `{ "message": "Could not create a Product. See the log file for details." }`;
-  
-//         if (product !== undefined) {
-//           resultStatusCode = 201;
-//           resultBody = JSON.stringify(product);
-//         }
-  
-//         return {
-//           statusCode: resultStatusCode,
-//           headers: {
-//             'Access-Control-Allow-Origin': '*'
-//           },
-//           body: resultBody
-//         };
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//       return {
-//         statusCode: 500,
-//         headers: {
-//           'Access-Control-Allow-Origin': '*'
-//         },
-//         body: `{ "message": "Internal Server Error. See the log file for details." }`
-//       };
-//     });
 }
